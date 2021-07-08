43926f9a3b1680f2a4c5900ae65230592a77449e
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2021-07-09 00:23:54 +0530

    sorting + pagination in backend

--- backend/app.py
+++ backend/app.py
@@ -1,60 +1,80 @@
 from flask import Flask, jsonify, abort, Response, request
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING, ASCENDING
 
-from dcol import DCol
+# from dcol import DCol
 
 app = Flask(__name__)
 client = MongoClient("mongodb://db/")
 systems_table = client.assets.systems
+MAX_RESULTS_PER_PAGE = 5
+
+
+def get_paginated_data(data: list, page_num: int) -> list:
+    if not data:
+        return data
+    if page_num * MAX_RESULTS_PER_PAGE <= len(data):
+        data = data[(page_num - 1) * MAX_RESULTS_PER_PAGE : page_num * MAX_RESULTS_PER_PAGE]
+    elif page_num * MAX_RESULTS_PER_PAGE > len(data) and (page_num - 1) * MAX_RESULTS_PER_PAGE <= len(data):
+        data = data[(page_num - 1) * MAX_RESULTS_PER_PAGE : len(data)]
+    else:
+        abort(404, description="No more results to show")
+    return data
 
 
 def get_response(data: list) -> Response:
     if not data:
-        abort(404, description="Invalid query, no items found!")
+        abort(404, description="No items found!")
     resp = jsonify(data)
     # resp.headers["X-Content-Type-Options"] = "nosniff"
     return resp
 
 
-@app.route("/api")
+@app.route("/api", methods=["GET"])
 def all_results() -> Response:
-    data = list(systems_table.find({}, {"_id": False}))
+    sort_key = request.args.get("sort") if request.args.get("sort") else "last_seen"
+    sort_order = ASCENDING if request.args.get("asc") == "1" else DESCENDING
+    page_num = int(request.args.get("page")) if request.args.get("page") else 1
+    data = list(systems_table.find({}, {"_id": False}, sort=[(sort_key, sort_order)]))
+    data = get_paginated_data(data, page_num)
     resp = get_response(data)
     return resp
 
 
-@app.route("/api/search", methods=["GET", "POST"])
+@app.route("/api/search", methods=["GET"])
 def search() -> Response:
-    categories = ["mac_address", "os", "domain", "workgroup", "ip_address", "hostname"]
+    categories = (
+        [request.args.get("category")]
+        if request.args.get("category")
+        else ["mac_address", "os", "domain", "workgroup", "ip_address", "hostname"]
+    )
+    query = request.args.get("query")
+    sort_key = request.args.get("sort") if request.args.get("sort") else "last_seen"
+    sort_order = ASCENDING if request.args.get("asc") == "1" else DESCENDING
+    page_num = int(request.args.get("page")) if request.args.get("page") else 1
+    if not query:
+        abort(404, description="No query passed")
     data = []
-    if request.method == "GET":
-        if request.args.get("category"):
-            categories = [request.args.get("category")]
-        query = request.args.get("query")
-    else:
-        if request.form["category"]:
-            categories = [request.form["category"]]
-        query = request.form["query"]
     for category in categories:
-        data = list(systems_table.find({category: query}, {"_id": False}))
+        data = list(systems_table.find({category: query}, {"_id": False}, sort=[(sort_key, sort_order)]))
         if data:
             break
+    data = get_paginated_data(data, page_num)
     resp = get_response(data)
     return resp
 
 
-@app.route("/api/dcol", methods=["POST"])
-def dcol_trigger() -> Response:
-    collector = DCol(request.form["ip"])
-    status = True
-    try:
-        collector.run()
-    except Exception as err:
-        print(err)
-        status = False
-    finally:
-        collector.close()
-    return jsonify({"success": status})
+# @app.route("/api/dcol", methods=["POST"])
+# def dcol_trigger() -> Response:
+#     collector = DCol(request.form["ip"])
+#     status = True
+#     try:
+#         collector.run()
+#     except Exception as err:
+#         print(err)
+#         status = False
+#     finally:
+#         collector.close()
+#     return jsonify({"success": status})
 
 
 @app.errorhandler(404)
